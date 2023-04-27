a35d7b4a545887d4de9491600108080833f31c2b
Luis Carlos <lucaldev22@gmail.com>
2023-04-27 14:26:06 -0300

    First footer commit.

---
+++ src/container/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as Styled from './styles';
+
+export default function Footer() {
+  return (
+    <Styled.Container>
+      <h1>Footer</h1>
+    </Styled.Container>
+  );
+}
