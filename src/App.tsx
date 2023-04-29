3115d1a1fd019742360f675f1cfe252cece9e602
Luis Carlos <lucaldev22@gmail.com>
2023-04-29 20:18:03 -0300

    forcing redeploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Header from './container/Header';
 import HomePage from './page/HomePage';
 import Footer from './container/Footer';
