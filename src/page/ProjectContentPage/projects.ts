3f5e14c537f562caa0d28e42cfca67a29b36e537
Luis Carlos <lucaldev22@gmail.com>
2023-05-16 23:03:48 -0300

    Adding projects.

--- src/page/ProjectContentPage/projects.ts
+++ src/page/ProjectContentPage/projects.ts
@@ -6,7 +6,7 @@ export const projects = [
     title: 'Portfolio',
     image: {
       url: images.portfolio,
-      alt: 'Imagem da frontpage do portfolio',
+      alt: 'Frontpage of the website portfolio',
     },
     description:
       'A Portifolio created using React.js, Typescript and Styled Components.',
@@ -27,10 +27,10 @@ export const projects = [
 
   {
     id: 1,
-    title: 'Contact list',
+    title: 'Contact list (CRUD)',
     image: {
       url: images.agenda,
-      alt: 'image agenda',
+      alt: 'Frontpage of the website Contact List',
     },
     description:
       'A notebook that allows you to save the contacts of your dear friends.',
@@ -42,7 +42,7 @@ export const projects = [
 
     The React Hook Form and YUP libraries were used to validate and create rules for user creation, login forms, contact creation and updating. In the Backend JWT was used to generate the token that allows the user to remain logged in after closing and reopening the page and Bcryptjs to encrypt the users' password.
 
-    The frontend is hosted on Vercel and the backend on Railway.
+    The frontend is deployed on Vercel and the backend on Railway.
     `,
     stack: 'React.js, Typescript, Node.js, Axios, Prisma e Styled Component',
     libs: 'React icons, react forms, yup e jwt',
@@ -56,28 +56,42 @@ export const projects = [
     id: 2,
     title: 'The Scions Report',
     image: {
-      url: images.nations,
-      alt: 'Imagem do site jogo das nações',
+      url: images.blog,
+      alt: 'Frontpage of the website The scions Report',
     },
-    description:
-      'A game website inspired on a very popular brazilian gamble called "Jogo do Bicho".',
-    content: `This project based on the popular game called "Jogo do Bicho" allows the user to guess what is the nation of the day.
+    description: 'A Final Fantasy XIV fanmade website (In development).',
+    content: `The goal with this blog its to show informations such as guides and news for the final fantasy xiv community.
 
-    For the frontend development it was used framer motion to do the animation of the theme button, the popover component from the Headlessui lib, was implemented to improve the acessibility and SCSS to style the website.
-
-    It was used React Redux to send the state that handles the theme change and using the localStorage was possible to save the theme preference and the previous countries that the user selected.
-
-    To make the daily draw, it was used a math operation involving the date components. The frontend is hosted on Vercel.
+    The frontend is developed on Next.js with the assistance of Typescript. The data is fetched using graphql and rendered with static props
     `,
-    stack: 'React.js, React Redux e SCSS',
-    libs: 'Headlessui, Framer motion e Phosphor icons',
+    stack: 'Next.js, Typescript, Styled Components',
+    libs: 'GraphQL, Jest and Phosphor icons. ',
     links: {
-      github: 'https://github.com/Lucal22/nations-game',
-      website: 'https://nations-game.vercel.app/',
+      github: 'https://github.com/Lucal22/tsr',
+      website: 'https://tsr-pi.vercel.app/',
     },
   },
   {
     id: 3,
+    title: 'Strapi - The Scions Report Backend',
+    image: {
+      url: images.strapipage,
+      alt: 'Imagem do strapi',
+    },
+    description: `Post manager for "The Scions Report" using Strapi.`,
+    content: `Strapi is an open-source headless CMS that allows developers to build and manage API-driven content easily. It provides a flexible and customizable environment for creating and organizing content, including structured data, media files, and more.
+
+    The app is deployed on Railway.
+    `,
+    stack: 'Strapi, Postgress',
+    libs: 'GraphQL, CKEditor',
+    links: {
+      github: 'https://github.com/Lucal22/blog-strapi-api',
+      website: '',
+    },
+  },
+  {
+    id: 4,
     title: 'Nations game',
     image: {
       url: images.nations,
@@ -91,7 +105,7 @@ export const projects = [
 
     It was used React Redux to send the state that handles the theme change and using the localStorage was possible to save the theme preference and the previous countries that the user selected.
 
-    To make the daily draw, it was used a math operation involving the date components. The frontend is hosted on Vercel.
+    To make the daily draw, it was used a math operation involving the date components. The frontend is deployed on Vercel.
     `,
     stack: 'React.js, React Redux e SCSS',
     libs: 'Headlessui, Framer motion e Phosphor icons',
@@ -101,7 +115,7 @@ export const projects = [
     },
   },
   {
-    id: 4,
+    id: 5,
     title: 'Keeper app',
     image: {
       url: images.keeper,
@@ -109,46 +123,13 @@ export const projects = [
     },
     description:
       'Save your daily tasks on the Keeper app, a to do list developed on react.js',
-    content: `O objetivo deste projeto foi criar uma lista de tarefas utilizando reactjs. Foi feito no ambiente de desenvolvimento do site CodeSandbox seguindo o curso da Dr. Angela Yu chamado "The Complete 2022 Web Development Bootcamp".`,
+    content: `The keeper app is a to do list developed with react.js on the CodeSandBox enviroment. This was my first project developed in react.js.
+    `,
     stack: 'React.js e CSS',
     libs: 'Material UI',
     links: {
-      github: 'https://github.com/Lucal22/blog-strapi-api',
+      github: 'https://github.com/Lucal22/Keeper-app',
       website: 'https://codesandbox.io/s/github/Lucal22/Keeper-app',
-    },
-  },
-
-  {
-    id: 5,
-    title: 'Blog pessoal - Frontend',
-    image: {
-      url: images.blog,
-      alt: 'Imagem do site Lucal Thoughts',
-    },
-    description: '',
-    content: ``,
-    stack: 'Nexts.js, Typescript, Tailwind Css, GraphQL',
-    libs: 'Phosphor icons',
-    links: {
-      github: 'https://github.com/Lucal22/lucal-thoughts',
-      website: 'https://lucal-thoughts.vercel.app/',
-    },
-  },
-  {
-    id: 6,
-    title: 'Blog pessoal - Backend',
-    image: {
-      url: images.strapipage,
-      alt: 'Imagem do strapi',
-    },
-    description: '',
-    content:
-      'Backend feito utilizando strapi para ser consumido pelo frontend do blog.',
-    stack: 'Strapi',
-    libs: '',
-    links: {
-      github: 'https://github.com/Lucal22/blog-strapi-api',
-      website: '',
     },
   },
 ];
