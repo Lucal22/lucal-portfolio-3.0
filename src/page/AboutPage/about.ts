25f4f34eb879aee78c8a6453ab8a51f1a2dd380c
Luis Carlos <lucaldev22@gmail.com>
2023-05-19 16:35:08 -0300

    Change on about text.

--- src/page/AboutPage/about.ts
+++ src/page/AboutPage/about.ts
@@ -14,6 +14,6 @@ export const about = {
 
   During the final year of my graduation, I had the opportunity to work as an intern for a multinational company's CAPEX engineering team. This role involved implementing significant projects within the company. It was an invaluable experience that allowed me to actively participate in all stages of a project, from its conceptualization to its successful initiation.
 
-  Subsequently, I served as a project engineer assistant for a company specializing in industrial projects. During this time, I diligently followed established schedules while actively contributing to the development and execution of various projects. This experience further honed my ability to collaborate effectively with different teams from diverse areas, ensuring the successful completion of each project.
+  Subsequently, I worked as a project engineer assistant for a company specializing in industrial projects. During this time, I diligently followed established schedules while actively contributing to the development and execution of various projects. This experience further honed my ability to collaborate effectively with different teams from diverse areas, ensuring the successful completion of each project.
   `,
 };
